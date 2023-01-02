7bb118b387b5fa534ce7f59144562e8ace31dd24
Ronaldo Lipata <lipata.ronaldo@gmail.com>
2023-01-02 23:04:56 +0800

    feat: add tag input from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
+import { useState } from 'react';
 import './App.css';
 
-function App() {
-  return <div className='App'></div>;
-}
+const App = ({ tags }) => {
+  const [tagData, setTagData] = useState(tags);
+  const removeTagData = (indexToRemove) => {
+    setTagData(tagData.filter((_, index) => index !== indexToRemove));
+  };
+  const addTagData = (event) => {
+    setTagData([...tagData, event.target.value]);
+    event.target.value = '';
+  };
+  return (
+    <div className='tag-input'>
+      <ul className='tags'>
+        {tagData.map((tag, index) => (
+          <li key={index} className='tag'>
+            <span className='tag-title'> {tag} </span>
+            <span
+              className='tag-close-icon'
+              onClick={() => removeTagData(index)}
+            >
+              x
+            </span>
+          </li>
+        ))}
+      </ul>
+      <input
+        type='text'
+        onKeyUp={(event) => (event.key === 'Enter' ? addTagData(event) : null)}
+        placeholder='Press enter to add a tag'
+      />
+    </div>
+  );
+};
 
 export default App;
